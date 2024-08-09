4e4aab5383acef9a60b71a70c4df84870a41437e
Devendra Gurjar <dev.viga@gmail.com>
2024-08-09 23:01:30 +0530

    clerk auth

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+
+import {
+  ClerkProvider,
+} from '@clerk/nextjs'
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Print Perfection Admin Dashboard",
+  description: "Admin Dashboard Page for Print_Perfection Webapp",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}

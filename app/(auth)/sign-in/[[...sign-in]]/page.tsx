4d8961255eff4dc079383bc68fa97e345f2ebefc
Devendra Gurjar <dev.viga@gmail.com>
2024-08-10 22:35:53 +0530

    add

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,7 @@
-import { SignIn } from "@clerk/nextjs";
+// import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <div className="h-screen flex justify-center items-center"><SignIn /></div>;
+  return <div className="h-screen flex justify-center items-center">
+    {/* <SignIn /> */}
+    </div>;
 }

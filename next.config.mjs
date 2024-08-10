179a28411862200fdffcc5d8f6038923286acd3e
Devendra Gurjar <dev.viga@gmail.com>
2024-08-10 23:07:04 +0530

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
-    // images: {
-    //     remotePatterns: [
-    //       {
-    //         hostname: 'res.cloudinary.com',
-    //       },
-    //     ],
-    //   },
+  output: 'export',
+    images: {
+        remotePatterns: [
+          {
+            hostname: 'res.cloudinary.com',
+          },
+        ],
+      },
 };
 
 export default nextConfig;
